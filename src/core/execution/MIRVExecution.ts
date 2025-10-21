7adccfe0207d33076cc298cb6c4b3e08c7bc3666
Loymdayddaud <145969603+TheGiraffe3@users.noreply.github.com>
2025-10-21 13:52:25 +0300

    Update MIRVExecution.ts

--- src/core/execution/MIRVExecution.ts
+++ src/core/execution/MIRVExecution.ts
@@ -68,7 +68,9 @@ export class MirvExecution implements Execution {
         this.active = false;
         return;
       }
-      this.nuke = this.player.buildUnit(UnitType.MIRV, spawn, {});
+      this.nuke = this.player.buildUnit(UnitType.MIRV, spawn, {
+        targetTile: this.dst,
+      });
       const x = Math.floor(
         (this.mg.x(this.dst) + this.mg.x(this.mg.x(this.nuke.tile()))) / 2,
       );
